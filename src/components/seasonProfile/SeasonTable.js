9a35c23f769f59d4222d3d7c0c9e906625b27a7f
Bryan Lilly <bryanjameslilly@gmail.com>
2022-03-11 21:18:42 +0000

    Link change

--- src/components/seasonProfile/SeasonTable.js
+++ src/components/seasonProfile/SeasonTable.js
@@ -1,4 +1,4 @@
-import { Link } from '@mui/material';
+import { Link } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
