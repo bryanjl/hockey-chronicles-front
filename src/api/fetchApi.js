13b15be9d6b91f5c2e83aa8df685d54669720998
Bryan Lilly <bryanjameslilly@gmail.com>
2022-03-17 08:54:57 +0000

    changed fetch url for prototype

--- src/api/fetchApi.js
+++ src/api/fetchApi.js
@@ -1,5 +1,5 @@
-// const baseUrl = 'https://hockey-chronicles-api.herokuapp.com/api/v1';
-const baseUrl = 'http://localhost:5000/api/v1';
+const baseUrl = 'https://hockey-chronicles-api.herokuapp.com/api/v1';
+// const baseUrl = 'http://localhost:5000/api/v1';
 
 
 const fetchApi = async (route, options) => {
